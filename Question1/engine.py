9172b5015a3dae98f9f32e81be7e9a3f516691d8
amini.reza <amini.reza@digikala.com>
2022-06-01 20:09:31 +0430

    :beers: add data loader

--- Question1/engine.py
+++ Question1/engine.py
@@ -1,6 +1,18 @@
 import logging
+import argparse
+
+from services import DataLoader
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
+parser = argparse.ArgumentParser(description="Describe How to Build and Run")
+parser.add_argument(
+    '--data_path',
+    type=str,
+    help="set relative path of data files",
+    required=False,
+    default='../data'
+)
+
 if __name__ == '__main__':
-    pass
+    csv_files = DataLoader(parser.parse_args().data_path)
--- Question1/services/data_loader.py
+++ Question1/services/data_loader.py
@@ -1,7 +1,31 @@
+import os
 
 
 class DataLoader:
+    DIRECTORY_SPLITTER = '/'
+    FILENAME_SPLITTER = '.'
+    FIRST_PART = 0
+    LAST_PART = -1
 
-    def __init__(self, files_path):
-        self.files_path = files_path
+    def __init__(self, files_path: str):
+        cwd = os.getcwd()
 
+        self.files_path = os.path.abspath(
+            os.path.join(cwd, files_path)
+        )
+
+    def get_files(self) -> list:
+        return [os.path.join(self.files_path, name) for name in os.listdir(self.files_path)]
+
+    def extract_filenames(self) -> dict:
+        files_paths = self.get_files()
+        file_names = dict()
+        for file_path in files_paths:
+            file_names[DataLoader.get_filename(file_path)] = file_path
+        return file_names
+
+    @staticmethod
+    def get_filename(filename: str) -> str:
+        return (filename.split(DataLoader.DIRECTORY_SPLITTER)[DataLoader.LAST_PART]).split(
+            DataLoader.FILENAME_SPLITTER
+        )[DataLoader.FIRST_PART]
