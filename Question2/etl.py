8c9aac1a3aa3f559306e0929ff22db4233edb2c1
amini.reza <amini.reza@digikala.com>
2022-06-05 00:33:38 +0430

    :beers: etl users data complete

--- Question2/etl.py
+++ Question2/etl.py
@@ -4,7 +4,7 @@ import datetime
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import DateType
-from pyspark.sql.functions import col, regexp_replace, date_format, udf
+from pyspark.sql.functions import col, regexp_replace, udf, lit, explode, array
 
 udf_time_convert = udf(lambda x: time_convert(x), DateType())
 
@@ -53,36 +53,32 @@ def extract(sc, bucket_name, raw_data_path):
     return sc.read.json(uri)
 
 
-def transform(df, is_tweets):
+def transform(df, is_tweets, sc):
     """ Transform dataframe to an acceptable form.
 
     Args:
         df: raw dataframe
+        is_tweets: bool for select tweets or users
 
     Return:
         df: processed dataframe
     """
     if is_tweets:
-        df = extract_tweets_fields(df)
+        df = transform_tweets(df, sc)
     else:
-        df = extract_users_fields(df)
+        df = transform_users(df)
 
     return df
 
 
-def extract_tweets_fields(df):
-    add_fields_message = ['id', 'id_str', 'lang']
-    add_fields_user = [
-        'name', 'screen_name', 'location', 'description', 'url', 'protected',
-        'followers_count', 'friends_count', 'listed_count', 'created_at', 'favourites_count',
-        'statuses_count', 'profile_image_url_https'
-    ]
+def transform_users(df):
+    add_fields_message = ['id', 'id_str', 'lang', 'name', 'screen_name', 'location', 'description', 'url', 'protected',
+                          'followers_count', 'friends_count', 'listed_count', 'created_at', 'favourites_count',
+                          'statuses_count', 'profile_image_url_https'
+                          ]
 
     for field in add_fields_message:
         df = df.withColumn(field, col(f'message.{field}'))
-
-    for field in add_fields_user:
-        df = df.withColumn(field, col(f'message.user.{field}'))
 
     remove_space_fields = ['description', 'url', 'name', 'location']
     for field in remove_space_fields:
@@ -94,6 +90,7 @@ def extract_tweets_fields(df):
         df = df.drop(field)
 
     df = df.withColumn('created_at', udf_time_convert(col("created_at")))
+    df = df.dropDuplicates(['id'])
     return df
 
 
@@ -102,7 +99,33 @@ def time_convert(time):
     return datetime.datetime.strptime(time, time_format)
 
 
-def extract_users_fields(df):
+def add_missing_columns(df, ref_df):
+    for col in ref_df.schema:
+        if col.name not in df.columns:
+            df = df.withColumn(col.name, lit(None).cast(col.dataType))
+    df = df.drop('entities')
+    return df
+
+
+def transform_tweets(df, sc):
+    df = df.drop('user')
+    df = df.select('message')
+    df = df.select(explode(array(col('message'))).alias('data')).select('data.*')
+    new = df.filter(col("status.retweeted_status").isNotNull())
+    new = new.select('status.retweeted_status')
+    new = new.select(explode(array(col('retweeted_status'))).alias('data')).select('data.*')
+    new = add_missing_columns(new, df)
+    df = add_missing_columns(df, new)
+    df.unionByName(new)
+    df = df.dropDuplicates(['id'])
+    df = df.withColumn('created_at', udf_time_convert(col("created_at")))
+    columns = [item[0] for item in df.dtypes if item[1].startswith('string')]
+    for column in columns:
+        df = df.withColumn(column, regexp_replace(col(column), " ", ""))
+    array_cols = ['status', 'friends_dic', 'followers_dic', 'place', 'extended_entities']
+    for c in array_cols:
+        df = df.drop(c)
+    df = df.repartition('created_at')
     return df
 
 
@@ -119,7 +142,8 @@ def load(df, bucket_name, processed_data_path):
          Nothing!
     """
     # todo: change this function if
-    df.write.csv('s3a://' + os.path.join(bucket_name, processed_data_path), header=True)
+    df.write.partitionBy("created_at") \
+        .mode("overwrite").csv('s3a://' + os.path.join(bucket_name, processed_data_path), header=True)
 
 
 @click.command('ETL job')
@@ -140,11 +164,16 @@ def main(appname, sparkmaster, minio_url,
     sc = init_spark_connection(appname, sparkmaster, minio_url,
                                minio_access_key, minio_secret_key)
 
+    if is_tweets in ['False', 'false', 'f', 0]:
+        is_tweets = False
+    else:
+        is_tweets = True
+
     # extract data from MINIO
     df = extract(sc, bucket_name, raw_data_path)
 
     # transform data to desired form
-    clean_df = transform(df, is_tweets)
+    clean_df = transform(df, is_tweets, sc)
 
     # load clean data to MINIO
     load(clean_df, bucket_name, processed_data_path)
